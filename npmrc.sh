6cf9f2f9729e84f572a087af0a1fb72aeb4a6771
Jen <6406037+usrrname@users.noreply.github.com>
2022-05-11 20:10:20 -0400

    build(azure-pipelines.yml): bump version before publishing

--- npmrc.sh
+++ npmrc.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env bash
 echo "${PACKAGE_SCOPE}/registry=https://pkgs.dev.azure.com/${ORG_NAME}/${PROJECT_NAME}/_packaging/${FEED_NAME}/npm/registry/
-registry.npmjs.org/:_authToken=${NPM_TOKEN}
+/registry.npmjs.org/:_authToken=${NPM_TOKEN}
 always-auth=true
 ; begin auth token
 //pkgs.dev.azure.com/${ORG_NAME}/_packaging/${FEED_NAME}/npm/registry/:username=radius2
